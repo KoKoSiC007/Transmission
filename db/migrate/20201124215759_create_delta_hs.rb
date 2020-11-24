7238c01ebf09257630afe936e063ad32ee0bf10c
KoKoSiC007 <okint34@yandex.ru>
2020-11-25 01:06:12 +0300

    Added reference on DeltaH to TeethHardness and ToothType

--- db/migrate/20201124215759_create_delta_hs.rb
+++ db/migrate/20201124215759_create_delta_hs.rb
@@ -1,6 +1,8 @@
 class CreateDeltaHs < ActiveRecord::Migration[6.0]
   def change
     create_table :delta_hs do |t|
+      t.belongs_to :teeth_hardness, foreign_key: true, type: :integer
+      t.belongs_to :tooth_type, foreign_key: true, type: :integer
       t.float :value, null: false
 
       t.timestamps
