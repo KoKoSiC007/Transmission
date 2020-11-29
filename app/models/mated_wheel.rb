710b0ac147128b2d4c0a11eba610d1a826a75237
KoKoSiC007 <okint34@yandex.ru>
2020-11-29 23:27:34 +0300

    Created calc func on MatedWheel

--- app/models/mated_wheel.rb
+++ app/models/mated_wheel.rb
@@ -1,2 +1,7 @@
 class MatedWheel < ApplicationRecord
+
+  def calc(v, e1, e2)
+    create(value: Math.sqrt((1/(Math.PI * (1 - v*v))) * (2 * e1 * e1 / (e1 + e2))))
+  end
 end
+
--- app/models/shape_mating_surface.rb
+++ app/models/shape_mating_surface.rb
@@ -3,6 +3,6 @@ class ShapeMatingSurface < ApplicationRecord
   belongs_to :offset_ratio, class_name: 'OffsetRatio', optional: true
 
   def calc(bb, atw)
-    Math.sqrt(2 * Math.cos(bb)/ Math.sin(2 * atw))
+    create(value: Math.sqrt(2 * Math.cos(bb)/ Math.sin(2 * atw)))
   end
 end
