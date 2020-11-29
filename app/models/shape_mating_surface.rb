85cf1d43637dd58c293fdf3332bbfe517bb95df1
KoKoSiC007 <okint34@yandex.ru>
2020-11-29 23:20:45 +0300

    Created calc func on ShapeMatingSurface

--- app/models/shape_mating_surface.rb
+++ app/models/shape_mating_surface.rb
@@ -1,4 +1,8 @@
 class ShapeMatingSurface < ApplicationRecord
-  belongs_to :tooth_angle, class_name: 'ToothAngle'
-  belongs_to :offset_ratio, class_name: 'OffsetRatio'
+  belongs_to :tooth_angle, class_name: 'ToothAngle', optional: true
+  belongs_to :offset_ratio, class_name: 'OffsetRatio', optional: true
+
+  def calc(bb, atw)
+    Math.sqrt(2 * Math.cos(bb)/ Math.sin(2 * atw))
+  end
 end
