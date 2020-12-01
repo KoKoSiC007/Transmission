69e678c66d6753ce03f470e6b87ffc916036a8b3
KoKoSiC007 <okint34@yandex.ru>
2020-12-01 20:24:18 +0300

    Created calc func on Whv

--- app/models/whv.rb
+++ app/models/whv.rb
@@ -2,4 +2,10 @@ class Whv < ApplicationRecord
   belongs_to :v, class_name: 'V'
   belongs_to :g0, class_name: 'G0'
   belongs_to :delta_h, class_name: 'DeltaH'
+
+  def calc(delta_h, g0, v, aw, u)
+    value = delta_h * g0 * v * Math.sqrt(aw/ u)
+
+    create(value: value, v: v, g0: g0, delta_h: delta_h, aw: aw, u: u)
+  end
 end
